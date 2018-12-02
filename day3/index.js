7d5cece69f6afcf928287cfba8ff7765b1908e3d
Brandon <bmc2775@gmail.com>
2018-12-02 18:38:19 -0500

    day 3 template

---
+++ day3/index.js
@@ -0,0 +1,12 @@
+const parse = require("../dataLoader").parse;
+
+const day3part1 = (data) => {
+}
+
+const day3part2 = (data) => {
+  
+}
+
+const data = parse(`${__dirname}/data.txt`);
+console.log(`Day 3 Part 1 -- ${day3part1(data)}`);
+console.log(`Day 3 Part 2 -- ${day3part2(data)}`);
